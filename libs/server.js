19e1f22fe44f489efb9279c5b977dd8958c2b6c7
Gregers Gram Rygg <gregersrygg@gmail.com>
2012-10-14 18:39:56 +0200

    Moved database stuff to temperature-model and allow batch creating temperature data

--- libs/server.js
+++ libs/server.js
@@ -1,13 +1,6 @@
 var connect = require('connect')
   , http = require('http')
-  , db = require('nano')({
-    "url":  "http://nodejitsudb496426227801.iriscouch.com:5984/gregers_no",
-    "log":  function (id, args) {
-      console.log("DB gregers_no:", id, args);
-    }
-  })
-  , tempRev
-  , designRev;
+  , model = require('./temperature-model.js');
 
 var app = connect()
   .use(connect.favicon())
@@ -20,61 +13,21 @@ var app = connect()
     var tempData;
 
     if ("PUT" === req.method && "/temperature" === req.url) {
-      tempData = req.body;
-      tempData._rev = tempRev;
-      db.insert(tempData, "temperature", function (err, body) {
-        if (err) {
-          console.error("Error inserting temperature to database", tempData, err);
-          return;
-        }
-        tempRev = body.rev;
-
-        console.info("Temperature data saved to database", body);
-      });
+      model.create(req.body);
       res.end("");
     } else if ("GET" === req.method && "/temperature" === req.url) {
-      db.view('temperature', 'last', function (err, body) {
-        if (err) {
-          console.error("Error viewing temperature");
-        } else {
-          body.rows.forEach(function (doc) {
-            console.log("LAST TEMPERATURE", doc);
-            res.write(doc.value);
-          });
-        }
+      model.last(function (data) {
+        console.log("LAST TEMPERATURE", data);
+        res.end("Last temperature: " + data.temp + "C " + new Date(data.time));
       });
+    } else if ("GET" === req.method && "/temperature-stats" === req.url) {
+      model.stats(function (data) {
+        console.log("TEMPERATURE STATS", data);
+        res.end("Temperature stats. " + Object.keys(data).map(function (key, value) { return key + ": " + value.toString(); }).join(' ') );
+      });
+    } else {
+      res.end('Hello from Connect!\n');
     }
-
-
-    res.end('Hello from Connect!\n');
   });
-
-// get last temperature revision
-db.get("temperature", function (err, body) {
-  if (err) return;
-  tempRev = body._rev;
-});
-
-/*db.get("_design/temperature", function (err, body) {
-  if (err) return;
-  designRev = body._rev;
-});*/
-
-/*db.insert({
-  "views": {
-    "last": {
-      "map":  function (doc) {
-        emit([doc.temp, doc.time], doc._id);
-      }
-    },
-    _rev: designRev
-  }
-}, '_design/temperature', function (err, body) {
-  if (err) {
-    console.error("Error inserting design doc", err);
-  } else {
-    console.log("Successfully created design doc", body);
-  }
-});*/
 
 http.createServer(app).listen(3000);
---
+++ libs/temperature-model.js
@@ -0,0 +1,113 @@
+var db = require('nano')({
+    //"url":  "http://nodejitsudb496426227801.iriscouch.com:5984/gregers_no",
+    "url":  "http://127.0.0.1:5984/gregers_no",
+    "log":  function (id, args) {
+      console.log("DB gregers_no:", id, args);
+    }
+  });
+
+
+db.list(function checkDbAvailability(err, body) {
+  if (err) throw new Error("Could not connect to CouchDB");
+});
+
+db.get("_design/temperature", function (err, body) {
+  if (err && err.error === 'not_found') {
+    createViews();
+  } else if (err) {
+    log.error("Couldn't get views from DB");
+    throw err;
+  } else {
+    destroyViews(body._rev);
+  }
+});
+
+function destroyViews(rev) {
+  db.destroy("_design/temperature", rev, function (err, body) {
+    if (err) return;
+    createViews();
+  });
+}
+
+function createViews () {
+  var mapTemp = function (doc) {
+    if (doc.temp && doc.time) {
+      emit(null, {
+        temp: doc.temp,
+        time: doc.time
+      });
+    }
+  };
+
+  db.insert({
+    "views": {
+      "last": {
+        "map":  mapTemp,
+        "reduce": function (keys, values) {
+        	var latest, latestTime = 0;
+        	values.forEach(function (data) {
+        		if (data.time > latestTime) {
+        			latestTime = data.time;
+        			latest = data;
+        		}
+        	});
+        	return latest;
+        }
+      },
+      "stats": {
+        "map":  function (doc) {
+          if (doc.temp && doc.time) {
+            emit("temp",  doc.temp);
+          }
+        },
+        "reduce": "_stats"
+      }
+    }
+  }, '_design/temperature', function (err, body) {
+    if (err) {
+      console.error("Error inserting design doc", err);
+    } else {
+      console.log("Successfully created design doc", body);
+    }
+  });
+}
+
+module.exports = {
+	create: function create(tempData) {
+		if (Array.isArray(tempData)) {
+			tempData.forEach(function (data) {
+				create(data);
+			});
+			return;
+		}
+		console.log("Inserting temperature to DB: ", tempData);
+	  db.insert(tempData, null, function (err, body) {
+	    if (err) {
+	      console.error("Error inserting temperature to database", tempData, err);
+	      return;
+	    }
+
+	    console.info("Temperature data saved to database", body);
+	  });
+	},
+
+	last: function (callback) {
+		db.view('temperature', 'last', function (err, body) {
+      if (err) {
+        console.error("Error getting last temperature");
+      } else {
+        callback( body.rows.length ? body.rows[0].value : null);
+      }
+    });
+	},
+
+	stats: function (callback) {
+		db.view('temperature', 'stats', function (err, body) {
+      if (err) {
+        console.error("Error calculating temperature stats");
+      } else {
+      	callback( body.rows.length ? body.rows[0].value : null);
+      }
+    });
+	}
+};
